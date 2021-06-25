a36e29faa5785c84cbbc49e0ef4e6cc6875100bf
Pratyay Rudra <rudrapratyay1999@gmail.com>
2021-06-25 23:00:01 +0530

    feat: homescreen design and added

---
+++ src/app/home/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/api/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  title = 'pothole-detecter';
+  lat = 22.560160123867558;
+  lng = 88.39648161534278;
+  locationChosen = true;
+  potholes: any;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData = () => {
+    this.dataService.getData().subscribe((data: any) => {
+      this.potholes = data;
+    });
+  };
+
+  onChooseLocation(event: any) {
+    console.log(event);
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+    this.locationChosen = true;
+  }
+}
